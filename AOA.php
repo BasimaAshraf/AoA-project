73da8ba201484cc068ddd86e322046a846e597c6
sanober1234 <56435297+sanober1234@users.noreply.github.com>
2019-11-23 23:45:36 +0500

    sort

---
+++ AOA.php
@@ -0,0 +1,35 @@
+html>
+<head></head>
+<body>
+	<?php
+	
+	function partition(&$my_array,$low,$high)
+	{
+		$pivot=$my_array[($low+$high)/2];
+
+		while ($low <= $high) 
+		{        
+			while ($my_array[$low] < $pivot)             
+				$low++;
+			while ($my_array[$high] > $pivot)
+				$high--;
+			if ($low <= $high) {
+				swap($my_array[$low], $my_array[$high]);
+				$tmp = $my_array[$low];
+				$my_array[$low] = $my_array[$high];
+				$my_array[$high] = $tmp;
+				$low++;
+				$high--;
+			}
+		}
+		return $low;
+	}
+
+	function quickSort(&$my_array, $low, $high)
+	{
+		$index = partition($my_array,$low,$high);
+		if ($low < $index - 1)
+			quickSort($my_array, $low, $index - 1);
+		if ($index < $high)
+			quickSort($my_array, $index, $high);
+	}
